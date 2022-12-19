20d82fb545c1e02a932c0028dce2ef265cb91c2c
GolubevAndrey <golubev_andrei87@mail.ru>
2022-12-19 04:14:45 +0300

    Заполнен StudentService

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -10,6 +10,29 @@ import java.util.Map;
 public class StudentServiceImpl implements StudentService {
 
     private final Map<Long, Student> students = new HashMap<>();
-    private long number = 0;
+    private long idNumber = 0;
+
+    @Override
+    public Student addStudent(Student student) {
+        student.setId(idNumber++);
+        students.put(student.getId(), student);
+        return student;
+    }
+    @Override
+    public Student findStudent(long id) {
+        return students.get(id);
+    }
+    @Override
+    public Student editStudent(long id, Student student) {
+        if (!students.containsKey(id)) {
+            return null;
+        }
+        students.put(id, student);
+        return student;
+    }
+    @Override
+    public Student deleteStudent(long id) {
+        return students.remove(id);
+    }
 
 }
