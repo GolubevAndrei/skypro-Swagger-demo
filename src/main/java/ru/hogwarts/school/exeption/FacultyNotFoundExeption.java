60c74d946322dfcbae327b599583d066e1e9ade4
GolubevAndrey <golubev_andrei87@mail.ru>
2023-01-17 22:30:04 +0300

    ДЗ 3.4

--- src/main/java/ru/hogwarts/school/exeption/FacultyNotFoundExeption.java
+++ src/main/java/ru/hogwarts/school/exeption/FacultyNotFoundExeption.java
@@ -3,7 +3,7 @@ package ru.hogwarts.school.exeption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NO_CONTENT)
 public class FacultyNotFoundExeption extends RuntimeException{
 
     private final Long id;
