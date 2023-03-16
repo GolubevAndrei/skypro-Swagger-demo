e34c2f6d7730d4c0815e333c03a7c8475abb94ec
GolubevAndrey <golubev_andrei87@mail.ru>
2023-03-16 08:53:12 +0300

    Испраылено changelog b SQL

--- src/main/resources/liquibase/scripts/43DB.sql
+++ src/main/resources/liquibase/scripts/43DB.sql
@@ -5,4 +5,4 @@ CREATE INDEX student_name_idx ON student (name);
 
 
 --changeset GolubevAA:2
-CREATE INDEX color_name_idx ON facultiesbcghfdktyj (color, name);
+CREATE INDEX color_name_idx ON faculties (color, name);
